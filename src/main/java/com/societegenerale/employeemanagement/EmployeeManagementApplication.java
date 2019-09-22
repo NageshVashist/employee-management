7fffc753c2a3289e1c206f10e6036bb67603ff90
Nagesh Vashist <nagesh113041521@gmail.com>
2019-09-22 21:31:53 +0530

    Initial commit

---
+++ src/main/java/com/societegenerale/employeemanagement/EmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.societegenerale.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
